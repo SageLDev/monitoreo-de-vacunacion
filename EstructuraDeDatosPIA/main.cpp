f77423e66e4d0d898c5765e01a7bd4ff616b8b01
SageL <sagel.alba@gmail.com>
2022-09-15 03:01:39 -0500

    Recursos de alta de vacunas y clientes finalizada y funcionalidad basica de las ventanas

--- EstructuraDeDatosPIA/main.cpp
+++ EstructuraDeDatosPIA/main.cpp
@@ -4,13 +4,18 @@
 using namespace std;
 
 HWND ghDlg = 0;
+HINSTANCE hInstGlobal;
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK PrincipalProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK LoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK RegistroProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK VacunasAltaProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK ClientesAltaProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
 {
+	hInstGlobal = hInstance;
 	ghDlg = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_PANTALLA_INICIAL), 0, (DLGPROC)PrincipalProc);
 	ShowWindow(ghDlg, nCmdShow);
 	MSG msg;
@@ -49,9 +54,27 @@ BOOL CALLBACK PrincipalProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 
 		case WM_COMMAND:
 		{
-			case ID_USUARIO_INICIODESESION:
-				DialogBox(NULL, MAKEINTRESOURCE(IDD_INICIO_SESION), hWnd, (DLGPROC)LoginProc);
+			switch (LOWORD(wParam)) {
+			case ID_DATOS_GUARDARDATOS:
 				break;
+			case ID_DATOS_CARGARDATOS:
+				break;
+			case ID_USUARIO_INICIODESESION:
+				DialogBox(hInstGlobal, MAKEINTRESOURCE(IDD_USUARIO_INICIO_SESION), hWnd, (DLGPROC)LoginProc);
+				break;
+			case ID_USUARIO_REGISTRODEUSUARIO:
+				DialogBox(hInstGlobal, MAKEINTRESOURCE(IDD_USUARIO_REGISTRO), hWnd, (DLGPROC)RegistroProc);
+				break;
+			case ID_USUARIO_CERRARSESION:
+				break;
+
+			case ID_VACUNAS_ALTADEVACUNA:
+				DialogBox(hInstGlobal, MAKEINTRESOURCE(IDD_VACUNAS_ALTA), hWnd, (DLGPROC)VacunasAltaProc);
+				break;
+			case ID_CLIENTES_ALTADECLIENTE:
+				DialogBox(hInstGlobal, MAKEINTRESOURCE(IDD_CLIENTES_ALTA), hWnd, (DLGPROC)ClientesAltaProc);
+				break;
+			}
 
 
 		} break;
@@ -83,6 +106,14 @@ BOOL CALLBACK LoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 		case WM_COMMAND:
 		{
+			switch (LOWORD(wParam)) {
+				case BTN_ACEPTAR_USUARIO_INICIO:
+				break;
+
+				case BTN_CERRAR_USUARIO_INICIO:
+					EndDialog(hWnd, 0);
+					return true;
+			}
 
 		} break;
 		case WM_CLOSE:
@@ -96,3 +127,100 @@ BOOL CALLBACK LoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	return false;
 }
+
+BOOL CALLBACK RegistroProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+
+	} break;
+
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam)) {
+			case BTN_ACEPTAR_USUARIO_REGISTRO:
+				break;
+			case BTN_CERRAR_USUARIO_REGISTRO:
+				EndDialog(hWnd, 0);
+				return true;
+		}
+
+	} break;
+	case WM_CLOSE:
+	{
+		EndDialog(hWnd, 0);
+		return true;
+	}
+
+	}
+
+
+	return false;
+}
+
+BOOL CALLBACK VacunasAltaProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+
+	} break;
+
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam)) {
+			case BTN_ACEPTAR_VACUNAS_ALTA:
+				break;
+			case BTN_CERRAR_VACUNAS_ALTA:
+				EndDialog(hWnd, 0);
+				return true;
+		}
+
+	} break;
+	case WM_CLOSE:
+	{
+		EndDialog(hWnd, 0);
+		return true;
+	}
+
+	}
+
+
+	return false;
+}
+
+BOOL CALLBACK ClientesAltaProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+
+	} break;
+
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam)) {
+			case BTN_ACEPTAR_CLIENTES_ALTA:
+				break;
+			case BTN_CERRAR_CLIENTES_ALTA:
+				EndDialog(hWnd, 0);
+				return true;
+		}
+
+	} break;
+	case WM_CLOSE:
+	{
+		EndDialog(hWnd, 0);
+		return true;
+	}
+
+	}
+
+
+	return false;
+}
+
--- EstructuraDeDatosPIA/resource.h
+++ EstructuraDeDatosPIA/resource.h
@@ -4,25 +4,67 @@
 //
 #define IDD_PANTALLA_INICIAL            1
 #define IDD_MENU_PRINCIPAL              4
-#define IDD_DIALOG1                     105
-#define IDD_INICIO_SESION               105
-#define IDD_CERRAR_DIALOGO_PRINCIPAL    1002
-#define ID_CERRAR_INICIO                1003
+#define IDD_USUARIO_REGISTRO            105
+#define IDD_USUARIO_INICIO_SESION       107
+#define IDD_VACUNAS_ALTA                109
+#define IDD_CLIENTES_ALTA               111
+#define BTN_CERRAR_CLIENTES_ALTA        1003
+#define CLIENTES_ALTA_APELLIDO_PATERNO  1004
+#define CLIENTES_ALTA_CURP              1005
+#define CLIENTES_ALTA_APELLIDO_MATERNO  1006
+#define CLIENTES_ALTA_RFC               1007
+#define CLIENTES_ALTA_COLONIA           1008
+#define CLIENTES_ALTA_CIUDAD            1009
+#define CLIENTES_ALTA_TELEFONO          1010
+#define CLIENTES_ALTA_PERFIL_RIESGO     1011
+#define CLIENTES_ALTA_CIVIL             1012
+#define CLIENTES_ALTA_OCUPACION         1013
+#define CLIENTES_ALTA_NOMBRE            1014
+#define CLIENTES_ALTA_CALLE             1015
+#define BTN_CLIENTES_ALTA_IDENTIDAD     1016
+#define BTN_CLIENTES_ALTA_CARGAR_IDENTIDAD 1016
+#define CLIENTES_ALTA_ESTADO            1018
+#define CLIENTES_ALTA_NACIMIENTO        1019
+#define BTN_ACEPTAR_USUARIO_REGISTRO    1020
+#define BTN_ACEPTAR_USUARIO_INICIO      1021
+#define BTN_CERRAR_USUARIO_REGISTRO     1022
+#define CLIENTES_ALTA_SEXO              1022
+#define BTN_CERRAR_USUARIO_INICIO       1023
+#define BTN_ACEPTAR_VACUNAS_ALTA        1024
+#define BTN_CERRAR_VACUNAS_ALTA         1025
+#define BTN_ACEPTAR_CLIENTES_ALTA       1026
+#define REGISTRO_USUARIO                1027
+#define REGISTRO_NOMBRE                 1028
+#define REGISTRO_APELLIDO_PATERNO       1029
+#define REGISTRO_APELLIDO_MATERNO       1030
+#define REGISTRO_CONTRA                 1031
+#define INICIO_USUARIO                  1032
+#define INICIO_CONTRA                   1033
+#define VACUNAS_ALTA_TIPO               1034
+#define VACUNAS_ALTA_MARCA              1035
+#define VACUNAS_ALTA_DOSIS              1036
+#define VACUNAS_ALTA_CLAVE              1037
+#define VACUNAS_ALTA_PRECIO             1038
+#define VACUNAS_ALTA_DESCRIPCION        1039
+#define TN_CLIENTES_ALTA_IDENTIDAD      1040
 #define ID_USUARIO_INICIODESESION       40001
 #define ID_USUARIO_CERRARSESION         40002
 #define ID_VACUNAS_ALTADEVACUNA         40003
 #define ID_VACUNAS_BAJADEVACUNA         40004
 #define ID_CLIENTES_ALTADECLIENTE       40005
 #define ID_CLIENTES_BAJADECLIENTE       40006
+#define ID_USUARIO_REGISTRODEUSUARIO    40007
+#define ID_DATOS_GUARDARDATOS           40008
+#define ID_DATOS_CARGARDATOS            40009
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40007
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40010
+#define _APS_NEXT_CONTROL_VALUE         1041
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
